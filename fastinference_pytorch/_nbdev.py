3cd1b25ce3903a0ff7877f7da48f44b9f236ca1f
Scott Mueller <mueller_scott@yahoo.com>
2020-08-04 19:36:46 -0400

    Finish dataset

--- fastinference_pytorch/_nbdev.py
+++ fastinference_pytorch/_nbdev.py
@@ -2,44 +2,17 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"FillMissing": "00_tabular.ipynb",
-         "Categorize": "01b_transforms.data.ipynb",
-         "Normalize": "01a_transforms.vision.ipynb",
-         "apply_procs": "00_tabular.ipynb",
-         "TabularDataset": "00_tabular.ipynb",
-         "tabular_learner": "00_tabular.ipynb",
-         "load_image": "01a_transforms.vision.ipynb",
-         "PILBase": "01a_transforms.vision.ipynb",
-         "Image.Image.crop_pad": "01a_transforms.vision.ipynb",
-         "CropPad": "01a_transforms.vision.ipynb",
-         "RandomCrop": "01a_transforms.vision.ipynb",
-         "OldRandomCrop": "01a_transforms.vision.ipynb",
-         "Resize": "01a_transforms.vision.ipynb",
-         "RandomResizedCrop": "01a_transforms.vision.ipynb",
-         "RatioResize": "01a_transforms.vision.ipynb",
-         "image2tensor": "01a_transforms.vision.ipynb",
-         "ToTensor": "01a_transforms.vision.ipynb",
-         "IntToFloatTensor": "01a_transforms.vision.ipynb",
-         "load_data": "03_rebuild.ipynb",
-         "load_model": "03_rebuild.ipynb",
-         "get_tfm": "03_rebuild.ipynb",
-         "generate_pipeline": "03_rebuild.ipynb",
-         "make_pipelines": "03_rebuild.ipynb",
-         "get_image": "04_learner.ipynb",
-         "Learner": "04_learner.ipynb",
-         "noop": "05_utils.ipynb",
-         "apply": "05_utils.ipynb",
-         "retain_type": "05_utils.ipynb",
-         "retain_meta": "05_utils.ipynb",
-         "to_device": "05_utils.ipynb",
-         "tensor": "05_utils.ipynb"}
+index = {"noop": "00_utils.ipynb",
+         "apply": "00_utils.ipynb",
+         "retain_type": "00_utils.ipynb",
+         "retain_meta": "00_utils.ipynb",
+         "to_device": "00_utils.ipynb",
+         "tensor": "00_utils.ipynb",
+         "to_numpy": "00_utils.ipynb",
+         "Dataset": "01_dataset.ipynb"}
 
-modules = ["tabular.py",
-           "transforms/vision.py",
-           "transforms/data.py",
-           "rebuild.py",
-           "learner.py",
-           "utils.py"]
+modules = ["utils.py",
+           "dataset.py"]
 
 doc_url = "https://muellerzr.github.io/fastinference_pytorch/"
 
